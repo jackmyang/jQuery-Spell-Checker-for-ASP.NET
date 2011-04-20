42164081ff095cd848d748e3367afcfa7ece3761
Jack Yang <jackmyang@gmail.com>
2011-04-20 16:39:26 -0400

    initial commit with MVC source

---
+++ jquery.spellchecker.mvc/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jquery.spellchecker.mvc")]
+[assembly: AssemblyDescription("jQuery spell checker for .NET, thanks to Scott Scowden for his initial prototype in MVC.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MIT License : http://www.opensource.org/licenses/mit-license.php")]
+[assembly: AssemblyProduct("jquery.spellchecker.mvc")]
+[assembly: AssemblyCopyright("Copyright © 2011 Richard Willis, Jack Yang")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("6d4183c4-2863-4b80-b5ea-238b4c2f7f0e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
